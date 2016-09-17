55f67601b0627379e1b9b93b11290f797a28ba86
NewFuture <new_future@msn.cn>
2016-09-17 11:40:13 +0800

    :part_alternation_mark: better cache unit test

--- tests/library/CacheTest.php
+++ tests/library/CacheTest.php
@@ -10,15 +10,20 @@ class CacheTest extends TestCase
     {
         Cache::flush();
     }
-    
+
+    public function setUp()
+    {
+        echo(strtok($this->getName(), ' '));
+    }
+
     //单组测试数据
     public function singleProvider()
     {
         return array(
-            'string_value' => array('_test_key_s','test_value',0),
             'Number Value' => array('_test_key_n',1234577),
             'bool Value'   => array('_test_key_bool',true,1),
-            'array Value'  => array('_test_key_array',array('a',2,'c'=>3),60),
+            'string_value' => array('_test_key_s','test_value',0),
+            'array Value'  => array('_test_key_array',array('a',2,'c' => 3),60),
         );
     }
 
@@ -27,113 +32,130 @@ class CacheTest extends TestCase
     {
         return array(
             array(
-                array('_mkey1_s'=>'test_value','_mkey1_i'=>10),0
+                array('_mkey1_s' => 'test_value','_mkey1_i' => 10),0
             ),
             array(
-                array('_mkey2_s'=>'test_value2','_mkey2bool'=>true),
+                array('_mkey3_a' => array('test_value2',122),'_mkey3_bool' => true),1
             ),
             array(
-                array('_mkey3_a'=>array('test_value2',122),'_mkey3_bool'=>true),1
+                array('_mkey2_s' => 'test_value2','_mkey2bool' => true),
             ),
         );
     }
 
     /**
-    * @dataProvider singleProvider
-    */
-    public function testSet($key, $value, $exp=0)
+     * @dataProvider singleProvider
+     */
+    public function testSet($key, $value, $exp = 0)
     {
         $this->assertTrue(Cache::set($key, $value, $exp));
     }
 
     /**
-    * @dataProvider singleProvider
-    * @depends testSet
-    */
-    public function testGet($key, $value, $exp=0)
-    {
-        $this->assertSame($value, Cache::get($key), $key.'不一致');
-        
-        //测试过期失效
-        if ($exp > 0) {
-            if ($this->app->getConfig()->get('cache.type') === 'file') {
-                $_SERVER['REQUEST_TIME'] += $exp + 0.1;
-                $this->assertFalse(Cache::get($key), $key);
-            } elseif ($exp < 10) {
-                static::sleep($exp);
-                $this->assertFalse(Cache::get($key), $key);
-            }
-        }
-    }
-
-
-    /**
-    * @dataProvider multiProvider
-    */
-    public function testMSet($data, $exp=0)
+     * @dataProvider multiProvider
+     */
+    public function testMSet($data, $exp = 0)
     {
         $this->assertTrue(call_user_func_array(array('Cache', 'set'), func_get_args()));
     }
 
     /**
-    * @dataProvider multiProvider
-    * @depends testMSet
-    */
-    public function testMGet($data, $exp=0)
+     * @dataProvider singleProvider
+     * @depends testSet
+     */
+    public function testGet($key, $value, $exp = 0)
     {
-        $keys=array_keys($data);
+        $this->assertSame($value, Cache::get($key));
+    }
+
+    /**
+     * @dataProvider multiProvider
+     * @depends testMSet
+     */
+    public function testMultiGet($data, $exp = 0)
+    {
+        $keys = array_keys($data);
         $this->assertSame($data, Cache::get($keys));
-        
-        //测试过期失效
-        if ($exp > 0) {
-            foreach ($data as &$value) {
-                $value=false;
-            }
+    }
+
+
+    /**
+     * 测试过期失效
+     *
+     * @dataProvider singleProvider
+     * @depends testGet
+     * @depends testMultiGet
+     */
+    public function testExpire($key, $value, $exp = 0)
+    {
+        if ($exp > 0 && $exp < 10) {
             if ($this->app->getConfig()->get('cache.type') === 'file') {
-                //文件存储
-                $_SERVER['REQUEST_TIME']  += $exp + 0.1;
-                $this->assertSame($data, Cache::get($keys));
-            } elseif ($exp < 10) {
-                //内存存储
+                $_SERVER['REQUEST_TIME'] += $exp + 0.1;
+                $this->assertFalse(Cache::get($key), $key);
+            } else {
                 static::sleep($exp);
-                $this->assertSame($data, Cache::get($keys));
+                $this->assertFalse(Cache::get($key), $key);
             }
+        } else {
+            $this->assertSame($value, Cache::get($key), $key.'不应该过期');
         }
     }
 
     /**
-    * @dataProvider singleProvider
-    * @depends testGet
-    */
-    public function testDel($key, $value, $exp=0)
+     *测试过期失效
+     *
+     * @dataProvider multiProvider
+     * @depends testGet
+     * @depends testMultiGet
+     */
+    public function testMultiExpire($data, $exp = 0)
     {
-        $result=Cache::del($key);
+        $keys = array_keys($data);
+        if ($exp > 0 && $exp < 10) {
+            $data = array_fill_keys($keys, false);
+            if ($this->app->getConfig()->get('cache.type') === 'file') {
+                //文件存储
+                $_SERVER['REQUEST_TIME']  += $exp + 0.1;
+            } else {
+                //内存存储
+                static::sleep($exp);
+            }
+        }
+        $this->assertSame($data, Cache::get($keys));
+    }
+
+    /**
+     * @dataProvider singleProvider
+     * @depends testGet
+     */
+    public function testDel($key, $value, $exp = 0)
+    {
+        $result = Cache::del($key);
         if ($exp == 0) {
-            $this->assertEquals(true, $result);
+            $this->assertTrue($result);
         }
         $this->assertFalse(Cache::get($key));
     }
 
 
     /**
-    * @dataProvider singleProvider
-    * @depends testDel
-    */
-    public function testFlush($key, $value, $exp=0)
+     * @dataProvider singleProvider
+     * @depends testDel
+     */
+    public function testFlush($key, $value, $exp = 0)
     {
-        $key=uniqid('_t_cache_');
-        $this->assertEquals(true, Cache::handler()->set($key, 'test value'));
+        $key = uniqid('_t_cache_');
+        $this->assertTrue(Cache::handler()->set($key, 'test value'));
         $this->assertNotFalse(Cache::flush());
         $this->assertFalse(Cache::get($key));
     }
 
     protected static function sleep($time)
     {
-        usleep($time*1000000+100000);
-        // time_sleep_until(time()+$time+0.2);
-        // $time = $_SERVER['REQUEST_TIME_FLOAT']+$time+0.3;
-        // if ($time>time()) {
-        //     time_sleep_until($time);
-        // }
+        // usleep($time * 1000000 + 100000);
+        $time = $_SERVER['REQUEST_TIME'] + $time + 1;
+        if ($time > microtime(true)) {
+            time_sleep_until($time);
+        }
     }
 }
