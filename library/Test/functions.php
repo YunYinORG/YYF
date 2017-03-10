fac00514b62af01d079d093efb31f7e06d5a4101
NewFuture <github@newfuture.cc>
2017-03-10 16:06:45 +0800

    phpunit 版本 更新兼容

--- library/Test/functions.php
+++ library/Test/functions.php
@@ -8,6 +8,12 @@
  * @copyright 2015-2017 NewFuture@yunyin.org
  */
 
+//全局 PHPUnit_Framework_TestCase 兼容 PHPUnit6.0
+if (!class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
+//array_column 兼容 php5.3
 if (!function_exists('array_column')) {
     function array_column($array, $column_name)
     {
